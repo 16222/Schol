f0f98ef80f1f0b84ecba0c054849d96f3430dfb1
16222 <16222@burnside.school.nz>
2020-03-24 09:42:31 +1300

    Proved a way to insert variables into SQLite from Powershell using System.Data.SQLite

--- powershellTesting.ps1
+++ powershellTesting.ps1
@@ -3,6 +3,9 @@ $con = New-Object -TypeName System.Data.SQLite.SQLiteConnection
 $con.ConnectionString = "DataSource=powershellDatabase.db"
 $con.Open()
 $sql = $con.CreateCommand()
+$sql.CommandText = "INSERT INTO test (message) VALUES (@message)";
+$sql.Parameters.AddWithValue("@message", "asdaasdda");
+$sql.ExecuteNonQuery();
 $sql.CommandText = "SELECT * FROM test"
 $adapter = New-Object -TypeName System.Data.SQLite.SQLiteDataAdapter $sql
 $data = New-Object System.Data.DataSet
