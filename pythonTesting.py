2a08e1c0bc0361f4a498d671be49ae5e6b9ea6a6
16222 <16222@burnside.school.nz>
2020-05-07 12:25:43 +1200

    Apparently commenting out things in html doesn't make them disappear. Also I hadn't properly committed the changes to the templates, so this should rectify that.

--- pythonTesting.py
+++ pythonTesting.py
@@ -2,5 +2,5 @@ import sqlite3, sys, subprocess
 
 p = subprocess.Popen((["powershell.exe",
                         "./ADTesting.ps1"]),
-                         stdout = sys.stdout)
+                         stdout = sys.stdout) #i can execute the powershell script from python
 
