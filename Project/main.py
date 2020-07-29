8cf79a61ee567d565a13f2657be229da5615a170
16222 <16222@burnside.school.nz>
2020-07-29 12:02:29 +1200

    MANAGED TO LINK TO MICROSOFT AUTHENTICATION !!!!!!!!!!

--- Project/main.py
+++ Project/main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request, jsonify, abort, redirect, ses
 import sqlite3 #importing sqlite3 libraries
 import json #importing json libraries
 from flask_sqlalchemy import SQLAlchemy
-from msal import PublicClientApplication
 from flask_session import Session
-import msal
 import app_config
+import uuid
+#from msalDef import _load_cache, _save_cache, _build_msal_app, _get_token_from_cache
+from msal import ConfidentialClientApplication, PublicClientApplication, SerializableTokenCache
+
 
 app = Flask(__name__) #initialising server
 
@@ -28,8 +30,6 @@ class resultStorage(db.Model):
 
 db.create_all()
 
-
-
 @app.route('/')
 def home():
     if not session.get('user'):
@@ -38,7 +38,17 @@ def home():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    session["state"] = str(uuid.uuid4())
+    auth_url = _build_msal_app().get_authorization_request_url(
+        app_config.SCOPE,
+        state=session["state"],
+        redirect_uri=url_for("authorised", _external=True)
+    )
+    return "<a href='%s'>Login with Microsoft Identity</a>" % auth_url
+
+@app.route('/tokenget')
+def authorised():
+    return render_template('/home')
 
 @app.route('/query')
 def queryConstruction():
@@ -48,6 +58,31 @@ def queryConstruction():
 @app.route('/getDetails')
 def pullDetails():
     return render_template('queryCreation.html')
+
+def _load_cache():
+    cache = SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
+    return cache
+
+def _save_cache(cache):
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
+
+def _build_msal_app(cache=None):
+    return ConfidentialClientApplication(
+        app_config.CLIENT_ID, authority=app_config.AUTHORITY,
+        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
+
+def _get_token_from_cache(scope=None):
+    cache = _load_cache()  # This web app maintains one cache per session
+    cca = _build_msal_app(cache)
+    accounts = cca.get_accounts()
+    if accounts:  # So all account(s) belong to the current signed-in user
+        result = cca.acquire_token_silent(scope, account=accounts[0])
+        _save_cache(cache)
+        return result
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port='8080')
---
+++ Project/msalDef.py
@@ -0,0 +1,27 @@
+import msal
+import uuid
+from flask import session
+
+def _load_cache():
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
+    return cache
+
+def _save_cache(cache):
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
+
+def _build_msal_app(cache=None):
+    return msal.ConfidentialClientApplication(
+        app_config.CLIENT_ID, authority=app_config.AUTHORITY,
+        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
+
+def _get_token_from_cache(scope=None):
+    cache = _load_cache()  # This web app maintains one cache per session
+    cca = _build_msal_app(cache)
+    accounts = cca.get_accounts()
+    if accounts:  # So all account(s) belong to the current signed-in user
+        result = cca.acquire_token_silent(scope, account=accounts[0])
+        _save_cache(cache)
+        return result
