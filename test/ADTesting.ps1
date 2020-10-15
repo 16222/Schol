8d0a65d8f92533350765d6d340d27e1282fbdcc3
16222 <16222@burnside.school.nz>
2020-10-16 11:09:35 +1300

    removed unnecessary test folder

--- test/ADTesting.ps1
+++
@@ -1,2 +0,0 @@
-Import-Module AzureAD
-Connect-AzureAD
--- test/pythonTesting.py
+++
@@ -1,6 +0,0 @@
-import sqlite3, sys, subprocess
-
-p = subprocess.Popen((["powershell.exe",
-                        "powershellTesting.ps1"]),
-                         stdout = sys.stdout) #i can execute the powershell script from python
-
